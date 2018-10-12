ac692eee5df1f2227a700bdac99a838850ba959f
Remi Lehe <remi.lehe@normalesup.org>
2018-10-12 08:18:48 -0700

    Stylistic improvements

--- generate_index_html.py
+++ generate_index_html.py
@@ -1,39 +1,28 @@
-
-# coding: utf-8
-
-# In[9]:
-
-
 import bokeh
 from bokeh.plotting import output_file, figure, show
 from bokeh.embed import components
 from jinja2 import Template
-
-
-# In[10]:
-
-
 import pandas as pd
 
 
-# In[11]:
-
 
 title_and_files = [ 
-    ('Perf. single node', './logs_csv/per_n_nodes/cori_knl_alltests_nnode_1.csv'),
-    ('Perf. two node', './logs_csv/per_n_nodes/cori_knl_alltests_nnode_1.csv') ]
+    ('Perf. single node', 
+     './logs_csv/per_n_nodes/cori_knl_alltests_nnode_1.csv'),
+    ('Perf. two node', 
+     './logs_csv/per_n_nodes/cori_knl_alltests_nnode_2.csv') ]
 plots = []
 for title, file in title_and_files:
     df = pd.read_csv(file)
     df['date'] = pd.to_datetime(df['date'])
     # create a new plot with a datetime axis type
-    p = figure(plot_width=800, plot_height=250, x_axis_type="datetime")
+    p = figure(plot_width=300, plot_height=250, 
+               x_axis_type="datetime", tools="box_zoom,reset")
+    p.xaxis.axis_label = 'Date'
+    p.yaxis.axis_label = 'Execution time (s)'
     p.line(df['date'], df['step_time'], color='navy', alpha=0.5)
     div, script = components(p)
     plots.append((title, div, script))
-
-
-# In[4]:
 
 
 # Generate the HTML file
