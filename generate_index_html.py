64c2c61b28cdcfb5f310c3c041e99ca6171061c9
mthevenet <mthevenet@lbl.gov>
2018-11-12 07:25:09 -0800

    plot output timer in logscale

--- generate_index_html.py
+++ generate_index_html.py
@@ -28,8 +28,12 @@ input_file_list = df['input_file'].unique()
 for count, input_file in enumerate( input_file_list ):
     df_filtered = df[ df['input_file']==input_file ]
     y_data_allmax = df_filtered[y_axis].max()
-    fig = figure(width=250, plot_height=250, title=input_file.replace('automated_test_',''), 
-                 y_range=[0., 1.1*y_data_allmax], x_axis_type="datetime")
+    if input_file == '6_output_2ppc':
+        fig = figure(width=250, plot_height=250, title=input_file.replace('automated_test_',''), 
+                     x_axis_type="datetime", y_axis_type='log')
+    else:
+        fig = figure(width=250, plot_height=250, title=input_file.replace('automated_test_',''), 
+                     y_range=[0., 1.1*y_data_allmax], x_axis_type="datetime")
     # Ugly trick to have the legend outside the plot.
     # The last plot of the line is larger, and a fraction of 
     # it contains the legend...
