83e8d44a9553dfa1fb5d3e134f5f933b94226cc6
MaxThevenet <mthevenet@lbl.gov>
2019-10-23 10:14:33 -0700

    h5 to csv now done in generate_index_html.py

--- generate_index_html.py
+++ generate_index_html.py
@@ -10,7 +10,29 @@ dict_csv_files = {
     'Summit @ OLCF': './logs_csv/summit.csv'
     }
 
+dict_hdf5_files = {
+    'Cori @ NERSC' : './logs_hdf5/cori_results.h5',
+    'Summit @ OLCF': './logs_hdf5/summit_results.h5'
+    }
+
 html_file = 'index.html'
+
+# Convert hdf5 data to csv
+for machine in list( dict_hdf5_files.keys() ):
+    # Extract small data from data frame and write them to
+    # First, generate csv files
+    df = pd.read_hdf( dict_hdf5_files[machine] )
+    # One large file
+    df.loc[:,'step_time'] = pd.Series(df['time_running']/df['n_steps'], index=df.index)
+    # Make smaller dataframe with only data to be written to csv file
+    df_small = df.copy()
+    df_small.loc[ df_small['input_file']=='automated_test_6_output_2ppc', 'step_time'] = \
+        df_small[ df_small['input_file']=='automated_test_6_output_2ppc' ]['time_WritePlotFile']
+    df_small = df_small.loc[:, ['date', 'input_file', 'git_hashes', 'n_node',
+                                'n_mpi_per_node', 'n_omp', 'rep', 'start_date',
+                                'time_initialization', 'step_time'] ]
+    # Write to csv
+    df_small.to_csv( csv_file[machine] )
 
 # Generate the HTML file
 with open('templates/template_index.html') as f:
